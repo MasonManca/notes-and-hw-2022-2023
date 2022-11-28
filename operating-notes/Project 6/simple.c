3551e092ea3211572649e52e575fbf0aec0c99e3
Mason Manca <masonmanca@Masons-MacBook-Pro.local>
2022-11-28 13:19:37 -0800

    initializing OS project

---
+++ operating-notes/Project 6/simple.c
@@ -0,0 +1,32 @@
+/*
+Mason Manca
+CPSC 346 Section: 2
+Dr. Zhang
+Project 6
+*/
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+
+/* This function is called when the module is loaded. */
+int simple_init(void)
+{
+       printk(KERN_INFO "Loading Module\n");
+
+       return 0;
+}
+
+/* This function is called when the module is removed. */
+void simple_exit(void) {
+	printk(KERN_INFO "Removing Module\n");
+}
+
+/* Macros for registering module entry and exit points. */
+module_init( simple_init );
+module_exit( simple_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
